01a0b5ed6470637d1f0c838da3bb1568210692e8
Oliver Damm <oliver.damm@gmx.de>
2024-07-23 17:30:57 +0200

    add transpiler

--- example/ts_interop_example.dart
+++ example/ts_interop_example.dart
@@ -1,12 +1,25 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:ts_interop/src/config/config.dart';
+import 'package:ts_interop/src/transpiler/transpiler.dart';
 import 'package:ts_interop/ts_interop.dart';
 
 void main() {
-  final file = File('example/@babylonjs_core@7.15.1.json');
-  final content = file.readAsStringSync();
+  final inFile = File('example/@babylonjs_core@7.16.0.json');
+  final content = inFile.readAsStringSync();
   final json = jsonDecode(content);
   final package = readTypeScriptAst(json);
-  //print(package);
+
+  final transpiler = Transpiler();
+  final lib = transpiler.transpile(package, Config());
+
+  final emitter = DartEmitter.scoped();
+  final DartFormatter formatter = DartFormatter();
+
+  final outFile = File('example/babylonjs.dart');
+
+  outFile.writeAsStringSync(formatter.format(lib.accept(emitter).toString()));
 }
